dc6d953c881c660841ba880779d29b90c8916e9e
mimoun <mimouncadosch@gmail.com>
2013-08-01 09:09:21 -0500

    added functions to charater.rb

--- app/models/charater.rb
+++ app/models/charater.rb
@@ -4,4 +4,23 @@ class Charater < ActiveRecord::Base
   acts_as_gmappable :latitude => 'lat', :longitude => 'lng', :process_geocoding => false,
                   :address => "address", :normalized_address => "address",
                   :msg => "Sorry, not even Google could figure out where that is"
+
+  def gmaps4rails_infowindow  
+    "<h5>#{tipo}</h5>"+
+    "<h5>#{address}</h5>"+
+    "<h5>#{neighborhood}</h5>"+
+    "<h5>#{date}</h5>"
+  end
+
+  def gmaps4rails_marker_picture
+  {
+  "picture" => "/assets/#{tipo}.png",          
+   "width" => "40",           
+   "height" => "40"
+ }
+  end
+
+  def gmaps4rails_sidebar
+    tipo
+  end
 end
