9ced8b559ca2ee49940737b62954a80d10e64ad3
mimoun <mimouncadosch@gmail.com>
2013-08-01 08:50:43 -0500

    modified charater.rb

--- app/models/charater.rb
+++ app/models/charater.rb
@@ -1,8 +1,7 @@
 class Charater < ActiveRecord::Base
   attr_accessible :address, :date, :latitude, :longitude, :name, :tipo, :name, :description, :cedula, :neighborhood
 
-  acts_as_gmappable :latitude => 'lat', :longitude => 'lng', :process_geocoding => false,
-                  :address => "address", :normalized_address => "address",
+  acts_as_gmappable :latitude => 'lat', :longitude => 'lng',
                   :msg => "Sorry, not even Google could figure out where that is"
 
 def gmaps4rails_infowindow  
